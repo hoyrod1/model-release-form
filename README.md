4ba0c24541633b6a3692e482f54f7aa1f12cdd4e
Rodney St Cloud <hoyrod1@aol.com>
2025-02-20 23:33:05 -0500

    Initial commit for the online model release form

---
+++ README.md
@@ -0,0 +1,3 @@
+This will be the initial version for the online model release form.
+
+This online model release form will allow models to digitally fill out the form online and submit the form data to a data base and also email a copy of the signed model release form to the model.
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>STC Media Model Release Form</title>
+  <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+  <div class="container">
+    <h2 class="dba">Exotic Hard Body Production</h2>
+    <h3 class="corp-name">A division of S.T.C. Media Inc</h3>
+    <h2 class="form-name">Model Release Form</h2>
+    <form action="" method="post">
+    </form>
+  </div>
+</body>
+</html>
